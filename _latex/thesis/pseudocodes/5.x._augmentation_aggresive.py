9fc452e4e7b1589b827ad0bc8d760e3da9faea7e
previtus <previtus@gmail.com>
2017-07-07 16:37:31 +0900

    vertical horizontal confusion

--- _latex/thesis/pseudocodes/5.x._augmentation_aggresive.py
+++ _latex/thesis/pseudocodes/5.x._augmentation_aggresive.py
@@ -2,8 +2,8 @@ image_generator = ImageDataGenerator(
 	rotation_range = 10.0,
 	width_shift_range = 0.2,
 	height_shift_range = 0.2,
-	horizontal_flip = False,
-	vertical_flip = True,
+	horizontal_flip = True,
+	vertical_flip = False
 	shear_range=0.2,
 	zoom_range=0.2,   
 )
--- _latex/thesis/pseudocodes/5.x._augmentation_expand.py
+++ _latex/thesis/pseudocodes/5.x._augmentation_expand.py
@@ -2,6 +2,6 @@ image_generator = ImageDataGenerator(
 	rotation_range = 0,
 	width_shift_range = 0.1,
 	height_shift_range = 0.1,  
-	horizontal_flip = False,  
-	vertical_flip = True  
+	horizontal_flip = True,
+	vertical_flip = False
 )
